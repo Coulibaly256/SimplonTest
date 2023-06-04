0ecc48a80269a3657e24124adc38a826c688f04b
Coulibaly256 <coulyouss256@gmail.com>
2023-06-04 22:24:59 +0000

    nouvelle modification dans insert.php

--- insert.php
+++ insert.php
@@ -6,103 +6,151 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des participants</title>
     <link rel="stylesheet" href="style.css">
+    
     <style>
-        body{
+        body {
             min-height: 100vh;
-
             display: flex;
             justify-content: center;
             align-items: center;
         }
 
-        main.table {
+        main {
             width: 82vw;
             height: 90vh;
-            background-color: 
-            border-collapse: collapse;
+            background-color: #fff5;
+            backdrop-filter: blur(7px);
+            box-shadow: 0 .4rem .8rem #000000;
+            border-radius: .8rem;
+            overflow: auto;
         }
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #000000;
+
+        section h1 {
+            width: 100%;
+            height: 10%;
+            background-color: #fff4;
+            padding: .8rem 1rem;
+        }
+
+        section table {
+            width: 95%;
+            max-height: calc(89% - .8rem);
+            background-color: #fffb;
+            margin: .8rem auto;
+            border-radius: .6rem;
+            overflow: auto;
+        }
+
+        /* section table::-webkit-scrollbar-thumb {
+            border-radius: .5rem;
+            background-color: #0004;
+            visibility: hidden;
+        }
+
+        section table:hover::-webkit-scrollbar-thumb {
+            visibility: visible;
+        } */
+
+        table {
+            width: 100%;
+            overflow: auto;
+            margin: 0 auto;
+        }
+
+        table,
+        th,
+        td {
+            padding: 1rem;
+        }
+
+        thead th {
+            position: sticky;
+            top: 0;
+            left: 0;
+            background-color: #d5d1defe;
+        }
+
+        tbody tr:nth-child(even) {
+            background-color: #0000000b;
+        }
+
+        tbody tr:hover {
+            background-color: #fff6;
         }
     </style>
 </head>
+
 <body>
+    <?php
+    include "connexion.php";
 
-<?php
+    if (
+        isset($_POST["nom"]) &&
+        isset($_POST["prenom"]) &&
+        isset($_POST["telephone"]) &&
+        isset($_POST["email"]) &&
+        isset($_POST["password"]) &&
+        isset($_POST["gender"])
+    ) {
+        $nom = $_POST["nom"];
+        $prenom = $_POST["prenom"];
+        $telephone = $_POST["telephone"];
+        $email = $_POST["email"];
+        $mot_passe = $_POST["password"];
+        $sexe = $_POST["gender"];
 
-include "connexion.php";
+        $req = mysqli_query($link, "INSERT INTO user(nom, prenom, telephone, email, mot_passe, sexe) VALUES ('$nom','$prenom', '$telephone', '$email', '$mot_passe', '$sexe')");
 
- // Récupérer les informations des participants depuis la base de données
- $query = mysqli_query($link, "SELECT * FROM user");
-
-
-if(isset($_POST["nom"])
-&& isset($_POST["prenom"])
-&& isset($_POST["telephone"])
-&& isset($_POST["email"])
-&& isset($_POST["password"])
-&& isset($_POST["gender"])
-)
-
-{
-    $nom = $_POST["nom"];
-    $prenom = $_POST["prenom"];
-    $telephone = $_POST["telephone"];
-    $email = $_POST["email"];
-    $mot_passe = $_POST["password"];
-    $sexe = $_POST["gender"];
-
-
-
-    $req = mysqli_query($link, "insert into user(nom, prenom,telephone,email,mot_passe,sexe) values ('$nom','$prenom', '$telephone', '$email', '$mot_passe', '$sexe')");
-
-
-    if($req)
-    {
-        // echo "insertion effectuée";
-    }
-    else{
-        // echo "erreur d'insertion";
-    }
-}
-
-// Vérifier s'il y a des participants
-if (mysqli_num_rows($query) > 0){
-    // Afficher le tableau des participants
-    echo "<table>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Téléphone</th>
-                    <th>Email</th>
-                    <th>Sexe</th>  
-                </tr>
-            </thead>";
-            // Parcourir les resultats de la requête
-        while($row = mysqli_fetch_assoc($query)){
-            echo "<tbody>
-                    <tr>
-                        <td>".$row['nom']."</td>
-                        <td>".$row['prenom']."</td>
-                        <td>".$row['telephone']."</td>
-                        <td>".$row['email']."</td>
-                        <td>".$row['sexe']."</td>
-                    </tr>
-                </tbody>";
+        if ($req) {
+            // echo "insertion effectuée";
+        } else {
+            // echo "erreur d'insertion";
         }
-        
-        echo "</table>";
-    } else {
-        echo "Aucun participant enregistré.";
     }
 
-    //Fermer la connexion à la base de données
+    $query = mysqli_query($link, "SELECT * FROM user");
+
+    if (mysqli_num_rows($query) > 0) {
+        echo "
+        <main>
+            <section>
+                <h1>La liste des participants</h1>
+            </section>
+            <section>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Téléphone</th>
+                            <th>Email</th>
+                            <th>Sexe</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+        ";
+
+        while ($row = mysqli_fetch_assoc($query)) {
+            echo "
+            <tr>
+                <td>{$row['nom']}</td>
+                <td>{$row['prenom']}</td>
+                <td>{$row['telephone']}</td>
+                <td>{$row['email']}</td>
+                <td>{$row['sexe']}</td>
+            </tr>
+            ";
+        }
+
+        echo "
+                    </tbody>
+                </table>
+            </section>
+        </main>
+        ";
+    }
+
     mysqli_close($link);
-
-?>
-
+    ?>
 </body>
 </html>
